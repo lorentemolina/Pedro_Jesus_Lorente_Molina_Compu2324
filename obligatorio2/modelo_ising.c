06166f55018a0ccda91c5bef02a0011be5369017
lorente <lorentemolina@correo.ugr.es>
2024-04-24 13:07:51 +0200

    sistemasolarproteus

--- obligatorio2/modelo_ising.c
+++ obligatorio2/modelo_ising.c
@@ -1,70 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "gsl/rng/gsl_rng.h"
+#include <time.h>
 
-#define SEED 1649176165
-
-gsl_rng *tau;
-
-int dim = 30, iter = 200;
-float T = 0.01;
+#define dim 30 // Dimensiones del retículo
+#define iter 200 // Número de iteraciones
+#define T 0.01
 
 int rnd_int() {
-    float num = gsl_rng_uniform_int(tau, 2);
-    if (num == 0) return -1;
-    else if (num == 1) return 1;
-    return 0;
+    return rand() % 2 == 0 ? 1 : -1;
 }
 
 int main() {
-
-    tau = gsl_rng_alloc(gsl_rng_taus);
-    gsl_rng_set(tau, SEED);
+    srand(time(NULL)); // Semilla aleatoria
 
     FILE *data;
-    data = fopen("1649176165_30_0.01_200.txt", "w");
+    data = fopen("ising_data.dat", "w");
 
     int s[dim][dim];
 
+    // Generar configuración inicial del retículo
     for (int i = 0; i < dim; i++) {
         for (int j = 0; j < dim; j++) {
             s[i][j] = rnd_int();
-            s[dim-1][dim-1] = s[0][0];
-            s[i][dim-1] = s[i][0];
-            s[dim-1][j] = s[0][j];
-            fprintf(data, "%d\t", s[i][j]);
+            fprintf(data, "%d", s[i][j]); // Imprimir el primer elemento sin coma
+            if (j < dim - 1) { // Si no es el último elemento de la fila
+                fprintf(data, ","); // Imprimir una coma
+            }
         }
-        if (i != dim-1) fprintf(data, "\n");
+        fprintf(data, "\n");
     }
+    fprintf(data, "\n");
 
+    // Realizar las iteraciones
     for (int k = 0; k < iter; k++) {
         for (int l = 0; l < dim*dim; l++) {
-            int i = gsl_rng_uniform_int(tau, dim);
-            int j = gsl_rng_uniform_int(tau, dim);
-            int E = 2*s[i][j] * ( s[ ( dim + (i+1) % dim ) % dim ][j] + s[ ( dim + (i-1) % dim) % dim ][j] + s[i][ ( dim + (j+1) % dim) % dim ] + s[i][ ( dim + (j-1) % dim ) % dim ] );
-            float p = fminf(1.0, exp(-E/T) );
-            float r = gsl_rng_uniform(tau);
+            int i = rand() % dim;
+            int j = rand() % dim;
+            int E = 2 * s[i][j] * (s[(dim + (i+1) % dim) % dim][j] + s[(dim + (i-1) % dim) % dim][j] + s[i][(dim + (j+1) % dim) % dim] + s[i][(dim + (j-1) % dim) % dim]);
+            float p = fminf(1.0, exp(-E/T));
+            float r = (float)rand() / RAND_MAX;
             if (r < p) {
                 s[i][j] *= -1;
-                for (int i = 0; i < dim; i++) {
-                    for (int j = 0; j < dim; j++) {
-                        s[dim-1][dim-1] = s[0][0];
-                        s[i][dim-1] = s[i][0];
-                        s[dim-1][j] = s[0][j];
-                    }
+            }
+        }
+
+        // Escribir los datos en el archivo después de cada iteración
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                fprintf(data, "%d", s[i][j]); // Imprimir el primer elemento sin coma
+                if (j < dim - 1) { // Si no es el último elemento de la fila
+                    fprintf(data, ","); // Imprimir una coma
                 }
             }
-        }
-        for (int i = 0; i < dim; i++) {
             fprintf(data, "\n");
-            for (int j = 0; j < dim; j++) {
-                fprintf(data, "%d\t", s[i][j]);
-            }
         }
+        fprintf(data, "\n");
     }
 
     fclose(data);
-    gsl_rng_free(tau);
     return 0;
 }
